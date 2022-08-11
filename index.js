a3f48991d69d105371dd506944653783c9a6af64
naimulislamshakil <ms0413354@gmail.com>
2022-08-11 19:44:24 +0600

    create a api

--- index.js
+++ index.js
@@ -7,3 +7,35 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.amlvbxb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    const headerCollaction = client.db("flipkart_db").collection("allmenu");
+
+    app.get("/menu", async (req, res) => {
+      const result = await headerCollaction.find().toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close()
+  }
+};
+
+run().catch(console.dir());
+
+app.get("/", (req, res) => {
+  res.send("How are you?");
+});
+
+app.listen(port, () => {
+  console.log(`flipkart server side runing ${port}`);
+});
